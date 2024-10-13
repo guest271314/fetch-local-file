974406d2c29c4dea848eff42c3fccaf541afd38f
guest271314 <guest271314@gmail.com>
2024-10-13 11:29:56 -0700

    Only define fetchLocalFile if not already defined on globalThis

--- fetch-local-file.js
+++ fetch-local-file.js
@@ -1,34 +1,36 @@
-Object.assign(globalThis, {
-  fetchLocalFile: async (extensionPath, filePath) => {
-    async function generateIdForPath(path) {
-      return [
-        ...[
-          ...new Uint8Array(
-            await crypto.subtle.digest(
-              "SHA-256",
-              new TextEncoder().encode(path),
+if (!Object.hasOwn(globalThis, "fetchLocalFile")) {
+  Object.assign(globalThis, {
+    fetchLocalFile: async (extensionPath, filePath) => {
+      async function generateIdForPath(path) {
+        return [
+          ...[
+            ...new Uint8Array(
+              await crypto.subtle.digest(
+                "SHA-256",
+                new TextEncoder().encode(path),
+              ),
             ),
-          ),
-        ].map((u8) => u8.toString(16).padStart(2, "0")).join("").slice(0, 32),
-      ]
-        .map((hex) =>
-          String.fromCharCode(parseInt(hex, 16) + "a".charCodeAt(0))
-        )
-        .join(
-          "",
-        );
-    }
-    const message = await chrome.runtime.sendMessage(
-      await generateIdForPath(extensionPath),
-      filePath,
-    );
-    if (typeof message === "string") {
-      throw new TypeError(message);
-    }
-    return Uint8Array.from(Object.values(
-      message,
-    ));
-  },
-});
+          ].map((u8) => u8.toString(16).padStart(2, "0")).join("").slice(0, 32),
+        ]
+          .map((hex) =>
+            String.fromCharCode(parseInt(hex, 16) + "a".charCodeAt(0))
+          )
+          .join(
+            "",
+          );
+      }
+      const message = await chrome.runtime.sendMessage(
+        await generateIdForPath(extensionPath),
+        filePath,
+      );
+      if (typeof message === "string") {
+        throw new TypeError(message);
+      }
+      return Uint8Array.from(Object.values(
+        message,
+      ));
+    },
+  });
 
-console.log("fetchLocalFile() declared");
+  console.log("fetchLocalFile() declared");
+}
